f4f207bccfb85cd9a392e5ffffccc993fe25a041
Sakmongkhon 018 <aucss02@gamil.com>
2022-12-06 17:05:03 +0700

    up

--- vite-react-crud/src/components/layouts/RootLayout.jsx
+++ vite-react-crud/src/components/layouts/RootLayout.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 import Footer from '../partials/Footer'
 import Header from '../partials/Header'
 
-const RootLayout = () => {
+const RootLayout = ({ children }) => {
     return (
         <>
             <Header />
-            <Outlet />
+            {children}
             <Footer />
         </>
     )
